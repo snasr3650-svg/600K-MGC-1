5d93f8d1d15a3f1806605924792074d961793f30
snasr3650-svg <snasr3650@gmail.com>
2026-09-15 15:35:58 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,64 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-12",
+      "name": "12",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06441251949995674,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-whatsapp-image-2026-09-13-at-163526"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-whatsapp-image-2026-09-13-at-163526",
+      "name": "WhatsApp Image 2026-09-13 at 16.35.26",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 400,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
